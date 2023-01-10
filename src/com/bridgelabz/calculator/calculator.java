ff46fb378b3b244acb803ef0164e755d44f5570b
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-10 14:46:35 +0530

    Welcome to the Calculator Program

---
+++ src/com/bridgelabz/calculator/calculator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.calculator;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("  Welcome to the calculator program ");
+
+    }
+}
